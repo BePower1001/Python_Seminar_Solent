5b0bba0c84f93a43af3966c9ec228781174b8a24
BePower1001 <adrian.pytlak@outlook.com>
2025-10-22 19:54:04 +0100

    Updated activity_1.py

--- Week1/activity_1.py
+++ Week1/activity_1.py
@@ -1,4 +1,8 @@
-# Displaying output prompt
-print("Hello Solent World")
+# Displaying message to the screen:
 
-# This is a comment
+print("Welcome to QH0444!")
+print("In week 1 we will learn...")
+print("....How to use Git and GitHub")
+print("...How to output to the screen")
+print("...How to get user input")
+print("I hope you are enjoying the lesson this far")
