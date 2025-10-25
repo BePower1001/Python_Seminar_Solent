94c85c7fa61033f1db53e5b838dfa4b5de16491a
BePower1001 <adrian.pytlak@outlook.com>
2025-10-25 12:46:16 +0100

    Added code to demonstrate the use of an if and else statements

--- Week2/output_task.py
+++ Week2/output_task.py
@@ -3,3 +3,10 @@ book_name=input()
 if book_name == "adventure":
     print(f"I like {book_name} books")
 print("Finished reading book.")
+
+print("Please enter the activity to be performed.")
+activity=input()
+if activity == "calculate":
+    print(f"Performing: {activity}")
+else:
+print("Activity completed")
