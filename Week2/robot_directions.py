36ae0097580056fd45d59871e8f13549721ab642
BePower1001 <adrian.pytlak@outlook.com>
2025-10-25 13:13:22 +0100

    Robot directions 1stage

---
+++ Week2/robot_directions.py
@@ -0,0 +1,12 @@
+print("Towards which direction should I go (up, down, left or right?")
+direction = input()
+if direction == "up":
+    print("\t Im moving up")
+elif direction == "down":
+        print("\t Im moving down")
+elif direction =="left":
+        print("\t Im moving left")
+elif direction =="right":
+        print("\t Im moving right")
+
+
