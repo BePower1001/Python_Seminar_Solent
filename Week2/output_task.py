3a1926b6e858bbed7ea7d4cc4ffb4d9972aa9dd0
BePower1001 <adrian.pytlak@outlook.com>
2025-10-25 12:34:15 +0100

    First task

--- Week2/output_task.py
+++ Week2/output_task.py
@@ -1,1 +1,5 @@
-print("Test new week2")
+print("What type of book is this?")
+book_name=input()
+if book_name == "adventure":
+    print(f"I like {book_name} books")
+print("Finished reading book.")
